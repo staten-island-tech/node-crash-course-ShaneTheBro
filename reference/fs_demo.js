88207527d89f3af5fa8e4323636c7b7306562895
ShaneTheBro <50725203+ShaneTheBro@users.noreply.github.com>
2019-12-30 14:04:33 -0500

    done

---
+++ reference/fs_demo.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+//Create folder
+// fs.mkdir(path.join(__dirname, '/test'), {}, function(err){
+//     if (err) throw err;
+//     console.log('Folder created...');
+// });
+
+// Create and write to file
+
+// fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello world!', function(err){
+//     if (err) throw err;
+//     console.log('File written to...');
+
+//     //file append
+//     fs.appendFile(path.join(__dirname, '/test', 'hello.txt'), ' I love Node.js', function(err){
+//         if (err) throw err;
+//         console.log('File written to...');
+//     });
+// });
+
+
+//Read file
+
+fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) =>{
+    if (err) throw err;
+    console.log(data);
+});
+
+//Rename file
+
+fs.rename(path.join(__dirname, '/test', 'hello.txt'), path.join(__dirname, '/test', 'helloworld.txt'), (err, data) =>{
+    if (err) throw err;
+    console.log('File renamed...');
+});
---
+++ reference/os_demo.js
@@ -0,0 +1,27 @@
+const os = require('os');
+
+//Platform
+
+console.log(os.platform());
+
+// CPU arch
+
+console.log(os.arch());
+
+//CPU Core Info
+
+console.log(os.cpus());
+
+// Free memory
+console.log(os.freemem());
+
+// Total memory
+console.log(os.totalmem());
+
+//Home dir
+
+console.log(os.homedir());
+
+//Uptime
+
+console.log(os.uptime());
